7ef773fd510d7d07ebaeac9ea4ded5182cdc001d
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-09-14 11:47:04 +0100

    refactor: add understandable comments

--- calculateLocalCoherence.m
+++ calculateLocalCoherence.m
@@ -1,13 +1,19 @@
 % Function to calculate local coherence for each patch here
+% Input:
+%   patch - specific patch taken from an image
+% Output:
+%   coherence - local coherence based on eigenanalysis
 function coherence = calculateLocalCoherence(patch)
     % Calculating gradient here
     [gx, gy] = gradient(patch);
+
     gx = gx(:);
     gy = gy(:);
     G = [gx, gy];
     
     %Calculating SVD values
     svd_value = svd(G' * G);
+    
     s1 = svd_value(1);
     s2 = svd_value(2);
 
