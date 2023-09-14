c2b87bca97e76338e79ab9d770aa2bb019194c27
radiobenzene <aurangabadkaruditangshu@gmail.com>
2023-09-14 11:50:11 +0100

    refactor: add understandable comments

--- calculateLocalMetric.m
+++ calculateLocalMetric.m
@@ -1,4 +1,8 @@
 % Function to calculate local metric for each patch
+% Input:
+%   patch - specific patch taken from an image
+% Output:
+%   coherence - local metric based on eigenanalysis
 function Q_metric = calculateLocalMetric(patch)
     % Calculating gradient for each patch
     [gx, gy] = gradient(patch);
